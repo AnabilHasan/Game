4205bbb36781dc69fde8d50d0631dd13cc8353df
root <root@localhost>
2022-01-21 13:55:27 +0600

    auto publish 1642751726662

---
+++ main.py
@@ -0,0 +1,19 @@
+Name=input("What's your name? :")
+AgeNum=input("Your age:")
+age=int(AgeNum)
+g=[3,2,1,0]
+for m in g :
+    print(m)
+m=input('Number:')
+print(m + ' It was the luckiest number.  You Won An I phone')
+print('Please wait for a second...... ')
+print('Here your identity:')
+print("Your Name:"+Name)
+print("Your Age:"+age)
+kl = input('Am I right? ')
+H = ("Yes")
+if kl == "H" :
+    print(Okay)
+else:
+    print('You lost your iPhone')
+
